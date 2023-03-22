7675366d44f52e72baedd1fd8484a1adeb053882
Vladislav Kheynov <lcastory86@gmail.com>
2023-03-22 23:13:57 +0700

    feat(bottom_navigation): add simple bottom navigation widget refactor(room_card): change custom container to Card

--- lib/components/room_card.dart
+++ lib/components/room_card.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
-enum RoomStatus { waitingForPlayers, awaitingForAccept, started }
+String calculateMembersCountLabel(int count) {
+  if (count % 10 == 1 && count % 100 != 11) {
+    return "человек";
+  } else if (count % 10 >= 2 &&
+      count % 10 <= 4 &&
+      (count % 100 < 10 || count % 100 >= 20)) {
+    return "человека";
+  } else {
+    return "человек";
+  }
+}
+
+enum RoomStatus {
+  waitingForPlayers(name: "Ожидаем игроков", color: Colors.yellow),
+  awaitingForAccept(name: "Ожидаем подтверждения", color: Colors.red),
+  started(name: "Игра началась", color: Colors.green);
+
+  const RoomStatus({
+    required this.name,
+    required this.color,
+  });
+
+  final String name;
+  final Color color;
+}
 
 class RoomCard extends StatelessWidget {
-  const RoomCard(
-      {super.key,
-      required this.name,
-      required this.membersCount,
-      required this.status});
+  const RoomCard({
+    super.key,
+    required this.name,
+    required this.membersCount,
+    required this.status,
+  });
 
   final String name;
   final int membersCount;
@@ -15,20 +40,12 @@ class RoomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey.shade400),
-            borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-            // ignore: prefer_const_literals_to_create_immutables
-            boxShadow: [
-              const BoxShadow(
-                  color: Colors.grey,
-                  spreadRadius: 3,
-                  blurRadius: 7,
-                  offset: Offset(0, 7))
-            ],
-            color: Colors.white),
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 28),
+    return Card(
+      elevation: 5,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: Container(
+        padding: const EdgeInsets.all(15),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -36,21 +53,38 @@ class RoomCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(name),
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
                 const SizedBox(height: 10),
-                Text("$membersCount человек"),
+                Text(
+                  "$membersCount ${calculateMembersCountLabel(membersCount)}",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
               ],
             ),
             Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text(status.name),
-                const SizedBox(height: 10),
-                const Text("скоро начало")
+                Text(
+                  status.name,
+                  style: TextStyle(
+                    color: status.color,
+                    fontSize: 16,
+                  ),
+                ),
               ],
             )
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
